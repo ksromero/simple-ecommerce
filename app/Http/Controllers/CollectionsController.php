a4682bea78bfa2ec0f293bdf01a337390275d586
ksromero <ks.romero0@gmail.com>
2018-08-15 12:51:59 +0800

    test

--- app/Http/Controllers/CollectionsController.php
+++ app/Http/Controllers/CollectionsController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\OrderProductResourceCollection;
 class CollectionsController extends Controller
 {
     public function index(){
+        $test = '';
         $errorFound = false;
         $error = ['error' => 'No Results Found'];
         $products = Product::with('orders');
